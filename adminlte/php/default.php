1590d11cb92311afd9364655e1448e88d79fe1b8
SK <sk@utsukta.org>
2024-10-12 17:27:17 +0530

    updated adminlte 4

--- adminlte/php/default.php
+++ adminlte/php/default.php
@@ -6,25 +6,20 @@
   <title><?php if(x($page,'title')) echo $page['title'] ?></title>
   <?php if(x($page,'htmlhead')) echo $page['htmlhead'] ?>
 </head>
-<body class="dark-mode layout-navbar-fixed layout-fixed">
-<div class="wrapper">
-
-  <!-- Preloader -->
-  <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__wobble" src="https://hub.utsukta.org/photo/ec268a46-11ab-427f-a605-a54cb341a637-1.png" alt="Utsukta Hub" height="60" width="60">
-  </div>
+<body class="layout-fixed sidebar-expand-lg sidebar-mini sidebar-collapse bg-body-tertiary app-loaded">
+<div class="app-wrapper">
 
   <?php if(x($page,'nav')) echo $page['nav']; ?>
 
    <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper px-2 py-2">
+  <div class="app-main px-2 py-2">
       <?php if(x($page,'content')) echo $page['content']; ?>
   </div>
   <!-- /.content-wrapper -->
 
 
   <!-- Main Footer -->
-  <footer class="main-footer text-sm">
+  <footer class="app-footer text-sm">
     🖖 Live long and prosper.
     <div class="float-right d-none d-sm-inline-block">
     </div>
