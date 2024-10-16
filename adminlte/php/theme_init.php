c12e644b80a4d1ce736254a5cbac8f0380f50398
SK <sk@utsukta.org>
2024-10-16 17:51:55 +0530

    moved content to middle column

--- adminlte/php/theme_init.php
+++ adminlte/php/theme_init.php
@@ -25,7 +25,7 @@ head_add_css('/library/justifiedGallery/justifiedGallery.min.css');
 head_add_css('/library/datetimepicker/jquery.datetimepicker.css');
 head_add_css('/vendor/twbs/bootstrap-icons/font/bootstrap-icons.min.css');
 
-head_add_js('/view/theme/adminlte/js/jquery.min.js');
+//head_add_js('/view/theme/adminlte/js/jquery.min.js');
 head_add_js('/library/datetimepicker/jquery.datetimepicker.js');
 
 head_add_js('/library/justifiedGallery/jquery.justifiedGallery.min.js');
