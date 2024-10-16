ad57ca157c2e07a8167936d5e005698f2587ed6a
SK <sk@utsukta.org>
2024-10-16 18:15:41 +0530

    js libraries to default bs5

--- adminlte/php/theme_init.php
+++ adminlte/php/theme_init.php
@@ -12,7 +12,7 @@ head_add_css('/library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.
 head_add_css('/view/theme/adminlte/css/adminlte.min.css');
 /*head_add_css('/view/theme/adminlte/css/style.css');*/
 
-head_add_js('/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js');
+/*head_add_js('/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js');*/
 head_add_js('/library/bootbox/bootbox.min.js');
 head_add_js('/library/bootstrap-tagsinput/bootstrap-tagsinput.js');
 head_add_js('/library/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js');
