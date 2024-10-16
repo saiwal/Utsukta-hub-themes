6a73be36290a8617ef0f7a49d07fc6e00a0af560
SK <sk@utsukta.org>
2024-10-16 18:24:20 +0530

    js libraries to default bs5

--- adminlte/php/theme_init.php
+++ adminlte/php/theme_init.php
@@ -9,13 +9,13 @@ head_add_css('/vendor/twbs/bootstrap-icons/font/bootstrap-icons.min.css');
 head_add_css('/library/bootstrap-tagsinput/bootstrap-tagsinput.css');
 head_add_css('/library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css');
 
+head_add_css('https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/styles/overlayscrollbars.min.css')
 head_add_css('/view/theme/adminlte/css/adminlte.min.css');
-/*head_add_css('/view/theme/adminlte/css/style.css');*/
 
 head_add_js('/library/bootbox/bootbox.min.js');
+head_add_js('/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js');
 head_add_js('/library/bootstrap-tagsinput/bootstrap-tagsinput.js');
 head_add_js('/library/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js');
 
 head_add_js('view/theme/adminlte/js/adminlte.min.js');
-/*head_add_js('/view/theme/adminlte/js/custom.js');*/
-head_add_js('/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js');
+head_add_js('https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/browser/overlayscrollbars.browser.es6.min.js')
