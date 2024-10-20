ffd96cb9acdeb94d455f2f02191dbba4cd89cf71
SK <sk@utsukta.org>
2024-10-20 19:39:55 +0530

    fixing commentbox etc.

--- adminlte/php/theme_init.php
+++ adminlte/php/theme_init.php
@@ -1,14 +1,62 @@
 <?php
 use Zotlabs\Lib\Config;
 
-require_once('view/php/theme_init.php');
+head_add_css('/library/datetimepicker/jquery.datetimepicker.css');
+//head_add_css('/library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css');
+head_add_css('/view/css/default.css');
+//head_add_css('/view/theme/adminlte/css/adminlte.min.css');
+//head_add_css('/view/theme/adminlte/css/style.css');
+//head_add_css('/view/theme/adminlte/css/fork-awesome.min.css');
+
+#require_once('view/php/theme_init.php');
+require_once('include/plugin.php');
+
+head_add_css('/library/tiptip/tipTip.css');
+head_add_css('/library/jgrowl/jquery.jgrowl.css');
+head_add_css('/library/jRange/jquery.range.css');
+
+head_add_css('/view/css/conversation.css');
+head_add_css('/view/css/widgets.css');
+head_add_css('/view/css/colorbox.css');
+head_add_css('/library/justifiedGallery/justifiedGallery.min.css');
+head_add_css('/library/datetimepicker/jquery.datetimepicker.css');
+
+head_add_js('/view/theme/adminlte/js/jquery.min.js');
+head_add_js('/library/datetimepicker/jquery.datetimepicker.js');
+
+head_add_js('/library/justifiedGallery/jquery.justifiedGallery.min.js');
+head_add_js('/library/sprintf.js/dist/sprintf.min.js');
+
+head_add_js('/library/textcomplete/textcomplete.min.js');
+head_add_js('autocomplete.js');
+
+head_add_js('/library/jquery.timeago.js');
+head_add_js('/library/readmore.js/readmore.js');
+head_add_js('/library/jgrowl/jquery.jgrowl.min.js');
+
+head_add_js('/library/sjcl/sjcl.js');
+
+head_add_js('acl.js');
+head_add_js('webtoolkit.base64.js');
+head_add_js('main.js');
+head_add_js('crypto.js');
+head_add_js('/library/jRange/jquery.range.js');
+head_add_js('/library/colorbox/jquery.colorbox-min.js');
+
+head_add_js('/library/jquery.AreYouSure/jquery.are-you-sure.js');
+head_add_js('/library/tableofcontents/jquery.toc.js');
+head_add_js('/library/Sortable/Sortable.min.js');
+head_add_js('/vendor/desandro/imagesloaded/imagesloaded.pkgd.min.js');
+
+
+// require_once('view/php/theme_init.php');
 // Add your custom CSS files here.
 head_add_css('/vendor/twbs/bootstrap-icons/font/bootstrap-icons.min.css');
 
 head_add_css('/library/bootstrap-tagsinput/bootstrap-tagsinput.css');
 head_add_css('/library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css');
 
-/*head_add_css('/view/theme/adminlte/css/adminlte.min.css');*/
+head_add_css('/view/theme/adminlte/css/adminlte.min.css');
 head_add_css('/view/theme/adminlte/css/overlayscrollbar.min.css');
 
 head_add_js('/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js');
