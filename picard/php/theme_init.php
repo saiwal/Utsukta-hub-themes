27e410cbc1e5d15789df71740b98ee76ed217261
SK <sk@utsukta.org>
2024-10-23 07:52:18 +0530

    fixed reaction image size in adminlte

--- picard/php/theme_init.php
+++ picard/php/theme_init.php
@@ -3,27 +3,27 @@ use Zotlabs\Lib\Config;
 
 require_once('view/php/theme_init.php');
 // Add your custom CSS files here.
-head_add_css('/vendor/twbs/bootstrap-icons/font/bootstrap-icons.min.css');
+head_add_css('vendor/twbs/bootstrap-icons/font/bootstrap-icons.min.css');
 
-head_add_css('/library/bootstrap-tagsinput/bootstrap-tagsinput.css');
-head_add_css('/library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css');
-head_add_css('/view/theme/picard/style/vendor/fonts/ubuntu-mono/ubuntu-mono.css');
-head_add_css('/view/theme/picard/style/vendor/fonts/antonio/antonio.css');
+head_add_css('library/bootstrap-tagsinput/bootstrap-tagsinput.css');
+head_add_css('library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css');
+head_add_css('view/theme/picard/style/vendor/fonts/ubuntu-mono/ubuntu-mono.css');
+head_add_css('view/theme/picard/style/vendor/fonts/antonio/antonio.css');
+head_add_css('view/theme/picard/style/vendor/bootstrap/css/bootstrap.min.css');
 /*head_add_css('/vendor/twbs/bootstrap/dist/css/bootstrap.min.css');*/
-head_add_css('/view/theme/picard/style/vendor/bootstrap/css/bootstrap.min.css');
-head_add_css('/view/theme/picard/style/vendor/datatables.min.css');
-head_add_css('/view/theme/picard/style/vendor/datatables_extensions.min.css');
-head_add_css('/view/theme/picard/style/vendor/daterangepicker.min.css');
-head_add_css('/view/theme/picard/style/vendor/AdminLTE.min.css');
-head_add_css('/view/theme/picard/style/vendor/select2.min.css');
+head_add_css('view/theme/picard/style/vendor/datatables.min.css');
+head_add_css('view/theme/picard/style/vendor/datatables_extensions.min.css');
+head_add_css('view/theme/picard/style/vendor/daterangepicker.min.css');
+head_add_css('view/theme/picard/style/vendor/AdminLTE.min.css');
+head_add_css('view/theme/picard/style/vendor/select2.min.css');
 head_add_css('view/theme/picard/style/pi-hole.css');
-head_add_css('/view/theme/picard/style/themes/lcars-picard.css');
-head_add_css('/view/theme/picard/style/vendor/js-warn.css');
+head_add_css('view/theme/picard/style/themes/lcars-picard.css');
+head_add_css('view/theme/picard/style/vendor/js-warn.css');
 
-head_add_js('/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js');
-head_add_js('/library/bootbox/bootbox.min.js');
-head_add_js('/library/bootstrap-tagsinput/bootstrap-tagsinput.js');
-head_add_js('/library/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js');
+head_add_js('vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js');
+head_add_js('library/bootbox/bootbox.min.js');
+head_add_js('library/bootstrap-tagsinput/bootstrap-tagsinput.js');
+head_add_js('library/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js');
 
 head_add_js('view/theme/picard/scripts/pi-hole/js/footer.js');
 /*head_add_js('view/theme/picard/js/style/vendor/js-warn.css');*/
