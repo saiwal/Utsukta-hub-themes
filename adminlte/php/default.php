aa0513982d5ef231083b5feb4af33f38b7edb4ff
SK <sk@utsukta.org>
2024-11-05 04:01:02 +0530

    updated network pdl

--- adminlte/php/default.php
+++ adminlte/php/default.php
@@ -1,162 +1,166 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php if(x($page,'title')) echo $page['title'] ?></title>
-  <script>var baseurl="<?php echo z_root() ?>";</script>
-  <?php if(x($page,'htmlhead')) echo $page['htmlhead'] ?>
+  <title><?php if (x($page, 'title')) echo $page['title'] ?></title>
+  <script>
+    var baseurl = "<?php echo z_root() ?>";
+  </script>
+  <?php if (x($page, 'htmlhead')) echo $page['htmlhead'] ?>
 </head>
+
 <body class="layout-fixed sidebar-expand-lg sidebar-mini bg-body-tertiary app-loaded sidebar-collapse">
-<div class="app-wrapper">
+  <div class="app-wrapper">
 
-      <?php if(x($page,'topnav')) echo $page['topnav']; ?>
+    <?php if (x($page, 'topnav')) echo $page['topnav']; ?>
 
-      <?php if(x($page,'nav')) echo $page['nav']; ?>
+    <?php if (x($page, 'nav')) echo $page['nav']; ?>
 
-   <!-- Content Wrapper. Contains page content -->
-  <main class="app-main px-3 py-3">
-    <div class="row">
+    <!-- Content Wrapper. Contains page content -->
+    <main class="app-main px-3 py-3">
+      <div class="row">
 
-      <div class="col-md-4 d-md-block col-lg-4 col-xl-3">
-        <div class="offcanvas-md offcanvas-start" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
-          <div class="offcanvas-header">
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
-          </div>
-          <div class="offcanvas-body">
-            <div class="container">
-              <?php if(x($page,'aside')) echo $page['aside']; ?>
+        <div class="col-md-4 d-md-block col-lg-4 col-xl-3">
+          <div class="offcanvas-md offcanvas-start" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
+            <div class="offcanvas-header">
+              <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <div class="container">
+                <?php if (x($page, 'aside')) echo $page['aside']; ?>
+              </div>
             </div>
           </div>
-         </div>
-       </div>
+        </div>
 
-      <div class="col-12 col-md-8 col-lg-8 col-xl-6">
-        <?php if(x($page,'content')) echo $page['content']; ?>
+        <div class="col-12 col-md-8 col-lg-8 col-xl-6">
+          <?php if (x($page, 'content')) echo $page['content']; ?>
+        </div>
+
+        <div class="d-lg-block col-xl-3">
+          <?php if (x($page, 'right_aside')) echo $page['right_aside']; ?>
+        </div>
+
       </div>
+    </main>
+    <!-- /.content-wrapper -->
 
-      <div class="d-lg-block col-xl-3">
-        <?php if(x($page,'right_aside')) echo $page['right_aside']; ?>
+
+    <!-- Main Footer -->
+    <footer class="app-footer text-sm">
+      🖖 Live long and prosper.
+      <div class="float-right d-none d-sm-inline-block">
       </div>
+    </footer>
+  </div>
+  <!-- ./wrapper -->
+  <script>
+    // Color Mode Toggler
+    (() => {
+      "use strict";
 
-    </div>
-  </main>
-  <!-- /.content-wrapper -->
+      const storedTheme = localStorage.getItem("theme");
 
+      const getPreferredTheme = () => {
+        if (storedTheme) {
+          return storedTheme;
+        }
 
-  <!-- Main Footer -->
-  <footer class="app-footer text-sm">
-    🖖 Live long and prosper.
-    <div class="float-right d-none d-sm-inline-block">
-    </div>
-  </footer>
-</div>
-<!-- ./wrapper -->
-<script>
-// Color Mode Toggler
-(() => {
-  "use strict";
+        return window.matchMedia("(prefers-color-scheme: dark)").matches ?
+          "dark" :
+          "light";
+      };
 
-  const storedTheme = localStorage.getItem("theme");
+      const setTheme = function(theme) {
+        if (
+          theme === "auto" &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches
+        ) {
+          document.documentElement.setAttribute("data-bs-theme", "dark");
+        } else {
+          document.documentElement.setAttribute("data-bs-theme", theme);
+        }
+      };
 
-  const getPreferredTheme = () => {
-    if (storedTheme) {
-      return storedTheme;
-    }
+      setTheme(getPreferredTheme());
 
-    return window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? "dark"
-      : "light";
-  };
+      const showActiveTheme = (theme, focus = false) => {
+        const themeSwitcher = document.querySelector("#bd-theme");
 
-  const setTheme = function (theme) {
-    if (
-      theme === "auto" &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-      document.documentElement.setAttribute("data-bs-theme", "dark");
-    } else {
-      document.documentElement.setAttribute("data-bs-theme", theme);
-    }
-  };
+        if (!themeSwitcher) {
+          return;
+        }
 
-  setTheme(getPreferredTheme());
+        const themeSwitcherText = document.querySelector("#bd-theme-text");
+        const activeThemeIcon = document.querySelector(".theme-icon-active i");
+        const btnToActive = document.querySelector(
+          `[data-bs-theme-value="${theme}"]`
+        );
+        const svgOfActiveBtn = btnToActive.querySelector("i").getAttribute("class");
 
-  const showActiveTheme = (theme, focus = false) => {
-    const themeSwitcher = document.querySelector("#bd-theme");
+        for (const element of document.querySelectorAll("[data-bs-theme-value]")) {
+          element.classList.remove("active");
+          element.setAttribute("aria-pressed", "false");
+        }
 
-    if (!themeSwitcher) {
-      return;
-    }
+        btnToActive.classList.add("active");
+        btnToActive.setAttribute("aria-pressed", "true");
+        activeThemeIcon.setAttribute("class", svgOfActiveBtn);
+        const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
+        themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
 
-    const themeSwitcherText = document.querySelector("#bd-theme-text");
-    const activeThemeIcon = document.querySelector(".theme-icon-active i");
-    const btnToActive = document.querySelector(
-      `[data-bs-theme-value="${theme}"]`
-    );
-    const svgOfActiveBtn = btnToActive.querySelector("i").getAttribute("class");
+        if (focus) {
+          themeSwitcher.focus();
+        }
+      };
 
-    for (const element of document.querySelectorAll("[data-bs-theme-value]")) {
-      element.classList.remove("active");
-      element.setAttribute("aria-pressed", "false");
-    }
+      window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .addEventListener("change", () => {
+          if (storedTheme !== "light" || storedTheme !== "dark") {
+            setTheme(getPreferredTheme());
+          }
+        });
 
-    btnToActive.classList.add("active");
-    btnToActive.setAttribute("aria-pressed", "true");
-    activeThemeIcon.setAttribute("class", svgOfActiveBtn);
-    const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
-    themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
+      window.addEventListener("DOMContentLoaded", () => {
+        showActiveTheme(getPreferredTheme());
 
-    if (focus) {
-      themeSwitcher.focus();
-    }
-  };
-
-  window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", () => {
-      if (storedTheme !== "light" || storedTheme !== "dark") {
-        setTheme(getPreferredTheme());
+        for (const toggle of document.querySelectorAll("[data-bs-theme-value]")) {
+          toggle.addEventListener("click", () => {
+            const theme = toggle.getAttribute("data-bs-theme-value");
+            localStorage.setItem("theme", theme);
+            setTheme(theme);
+            showActiveTheme(theme, true);
+          });
+        }
+      });
+    })();
+  </script>
+  <script>
+    const SELECTOR_SIDEBAR_WRAPPER = ".sidebar-wrapper";
+    const Default = {
+      scrollbarTheme: "os-theme-light",
+      scrollbarAutoHide: "leave",
+      scrollbarClickScroll: true,
+    };
+    document.addEventListener("DOMContentLoaded", function() {
+      const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
+      if (
+        sidebarWrapper &&
+        typeof OverlayScrollbarsGlobal?.OverlayScrollbars !== "undefined"
+      ) {
+        OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
+          scrollbars: {
+            theme: Default.scrollbarTheme,
+            autoHide: Default.scrollbarAutoHide,
+            clickScroll: Default.scrollbarClickScroll,
+          },
+        });
       }
     });
-
-  window.addEventListener("DOMContentLoaded", () => {
-    showActiveTheme(getPreferredTheme());
-
-    for (const toggle of document.querySelectorAll("[data-bs-theme-value]")) {
-      toggle.addEventListener("click", () => {
-        const theme = toggle.getAttribute("data-bs-theme-value");
-        localStorage.setItem("theme", theme);
-        setTheme(theme);
-        showActiveTheme(theme, true);
-      });
-    }
-  });
-})();
-</script>
-   <script>
-        const SELECTOR_SIDEBAR_WRAPPER = ".sidebar-wrapper";
-        const Default = {
-            scrollbarTheme: "os-theme-light",
-            scrollbarAutoHide: "leave",
-            scrollbarClickScroll: true,
-        };
-        document.addEventListener("DOMContentLoaded", function() {
-            const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
-            if (
-                sidebarWrapper &&
-                typeof OverlayScrollbarsGlobal?.OverlayScrollbars !== "undefined"
-            ) {
-                OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
-                    scrollbars: {
-                        theme: Default.scrollbarTheme,
-                        autoHide: Default.scrollbarAutoHide,
-                        clickScroll: Default.scrollbarClickScroll,
-                    },
-                });
-            }
-        });
-    </script> <!--end::OverlayScrollbars Configure--> 
+  </script> <!--end::OverlayScrollbars Configure-->
 </body>
-</html>
 
+</html>
--- adminlte/tpl/navbar_default.tpl
+++ adminlte/tpl/navbar_default.tpl
@@ -5,7 +5,7 @@
     <!--begin::Brand Link-->
     <a href="/" class="brand-link">
       <!--begin::Brand Image-->
-<!--      <img
+      <!--      <img
         src="./assets/img/AdminLTELogo.png"
         alt="U"
         class="brand-image opacity-75 shadow"
@@ -22,30 +22,31 @@
   <div class="sidebar-wrapper">
     <nav class="mt-2">
       <!--begin::Sidebar Menu-->
-      <ul
-        class="nav sidebar-menu flex-column"
-        data-lte-toggle="treeview"
-        role="menu"
-        data-accordion="false"
-      >
-      <li class="nav-header">
-        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-          <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked data-bs-theme-value="auto">
-          <label class="btn btn-sm btn-outline-secondary" for="btnradio1"><i class="bi bi-circle-half me-2"></i>Auto</label>
+      <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
+        <li class="nav-header">
+          <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked
+              data-bs-theme-value="auto">
+            <label class="btn btn-sm btn-outline-secondary" for="btnradio1"><i
+                class="bi bi-circle-half me-2"></i>Auto</label>
 
-          <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off" data-bs-theme-value="dark">
-          <label class="btn btn-sm btn-outline-secondary" for="btnradio2"><i class="bi bi-moon-fill me-2"></i>Dark</label>
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off"
+              data-bs-theme-value="dark">
+            <label class="btn btn-sm btn-outline-secondary" for="btnradio2"><i
+                class="bi bi-moon-fill me-2"></i>Dark</label>
 
-          <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off" data-bs-theme-value="light">
-          <label class="btn btn-sm btn-outline-secondary" for="btnradio3"><i class="bi bi-sun-fill me-2"></i>Light</label>
-        </div>        
-      </li>
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off"
+              data-bs-theme-value="light">
+            <label class="btn btn-sm btn-outline-secondary" for="btnradio3"><i
+                class="bi bi-sun-fill me-2"></i>Light</label>
+          </div>
+        </li>
 
         <!-- Pinned user apps -->
         {{if $navbar_apps.0}}
         <li class="nav-header" aria-disabled="true">{{$pinned_apps}}</li>
         {{foreach $navbar_apps as $navbar_app}}
-          {{$navbar_app|replace:'fa':'generic-icons-nav fa'}}
+        {{$navbar_app|replace:'fa':'generic-icons-nav fa'}}
         {{/foreach}}
         {{/if}}
 
@@ -53,30 +54,32 @@
         {{if $channel_apps.0}}
         <li class="nav-header" aria-disabled="true">{{$channelapps}}</li>
         {{foreach $channel_apps as $channel_app}}
-          {{$channel_app}} <br>
+        {{$channel_app}} <br>
         {{/foreach}}
         {{/if}}
 
         {{if $is_owner}}
         <!-- Starred user apps -->
-        <li class="nav-item"> 
-        <a href="#" class="nav-link"> <i class="nav-icon bi bi-star"></i>
-          <p>{{$featured_apps}}<i class="nav-arrow bi bi-chevron-right"></i></p>
-        </a>
-        <ul class="nav nav-treeview" style="display: none; box-sizing: border-box;">
-        {{foreach $nav_apps as $nav_app}}
-          {{$nav_app}}
-        {{/foreach}}
-        </ul>
-        </li>        
-        <li class="nav-header"><a class="nav-link" href="/apps"><i class="bi bi-gear"></i><p>{{$addapps}}</p></a></li>
+        <li class="nav-item">
+          <a href="#" class="nav-link"> <i class="nav-icon bi bi-star"></i>
+            <p>{{$featured_apps}}<i class="nav-arrow bi bi-chevron-right"></i></p>
+          </a>
+          <ul class="nav nav-treeview" style="display: none; box-sizing: border-box;">
+            {{foreach $nav_apps as $nav_app}}
+            {{$nav_app}}
+            {{/foreach}}
+          </ul>
+        </li>
+        <li class="nav-header"><a class="nav-link" href="/apps"><i class="bi bi-gear"></i>
+            <p>{{$addapps}}</p>
+          </a></li>
         {{else}}
         <li class="nav-header" aria-disabled="true">{{$sysapps}}</li>
         <!-- System apps -->
         {{foreach $nav_apps as $nav_app}}
-          {{$nav_app}}
+        {{$nav_app}}
         {{/foreach}}
-        {{/if}} 
+        {{/if}}
       </ul>
       <!--end::Sidebar Menu-->
     </nav>
@@ -84,4 +87,3 @@
   <!--end::Sidebar Wrapper-->
 </aside>
 <!--end::Sidebar-->
-
