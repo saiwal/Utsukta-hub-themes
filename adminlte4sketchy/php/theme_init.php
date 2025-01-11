b3154fa25f2382bcae308e94bc30a44244e10ac2
SK <sk@utsukta.org>
2025-01-12 04:42:46 +0530

    added sketchy variant

---
+++ adminlte4sketchy/php/theme_init.php
@@ -0,0 +1,21 @@
+<?php
+use Zotlabs\Lib\Config;
+
+require_once('view/php/theme_init.php');
+// Add your custom CSS files here.
+head_add_css('/vendor/twbs/bootstrap-icons/font/bootstrap-icons.min.css');
+
+head_add_css('/library/bootstrap-tagsinput/bootstrap-tagsinput.css');
+head_add_css('/library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css');
+
+head_add_css('/view/theme/adminlte4/css/adminlte.min.css');
+head_add_css('/view/theme/adminlte4/css/bootstrap.min.css');
+head_add_css('/view/theme/adminlte4/css/overlayscrollbar.min.css');
+
+head_add_js('/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js');
+head_add_js('/library/bootbox/bootbox.min.js');
+head_add_js('/library/bootstrap-tagsinput/bootstrap-tagsinput.js');
+head_add_js('/library/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js');
+
+head_add_js('/view/theme/adminlte4/js/adminlte.min.js');
+head_add_js('/view/theme/adminlte4/js/overlayscrollbar.min.js');
---
+++ adminlte4sketchy/tpl/common_widget.tpl
@@ -0,0 +1,6 @@
+<div class="widget card mb-3">
+	<h5>
+		{{$title}}
+	</h5>
+	{{$content}}
+</div>
