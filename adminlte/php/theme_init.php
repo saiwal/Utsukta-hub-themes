047439f4c60544927324c3d0cd69f1d785138ccb
SK <sk@utsukta.org>
2025-01-14 16:43:18 +0530

    undo

--- adminlte/php/theme_init.php
+++ adminlte/php/theme_init.php
@@ -1,44 +1,7 @@
 <?php
 use Zotlabs\Lib\Config;
 
-/*require_once('view/php/theme_init.php');*/
-require_once('include/plugin.php');
-
-head_add_css('/library/jRange/jquery.range.css');
-
-head_add_css('/view/css/conversation.css');
-head_add_css('/view/css/widgets.css');
-head_add_css('/view/css/colorbox.css');
-head_add_css('/library/justifiedGallery/justifiedGallery.min.css');
-head_add_css('/library/datetimepicker/jquery.datetimepicker.css');
-
-head_add_js('jquery.js');
-head_add_js('/library/datetimepicker/jquery.datetimepicker.js');
-
-head_add_js('/library/justifiedGallery/jquery.justifiedGallery.min.js');
-head_add_js('/library/sprintf.js/dist/sprintf.min.js');
-
-head_add_js('/library/textcomplete/textcomplete.min.js');
-head_add_js('autocomplete.js');
-
-head_add_js('/library/readmore.js/readmore.js');
-
-head_add_js('/library/sjcl/sjcl.js');
-head_add_js('/library/sodium-plus/dist/sodium-plus.min.js');
-
-head_add_js('acl.js');
-head_add_js('webtoolkit.base64.js');
-// head_add_js('main.js');
-head_add_js('/view/theme/adminlte/js/script.js');
-head_add_js('crypto.js');
-head_add_js('/library/jRange/jquery.range.js');
-head_add_js('/library/colorbox/jquery.colorbox-min.js');
-
-head_add_js('/library/jquery.AreYouSure/jquery.are-you-sure.js');
-head_add_js('/library/tableofcontents/jquery.toc.js');
-head_add_js('/library/Sortable/Sortable.min.js');
-
-
+require_once('view/php/theme_init.php');
 // Add your custom CSS files here.
 head_add_css('/vendor/twbs/bootstrap-icons/font/bootstrap-icons.min.css');
 
@@ -48,7 +11,6 @@ head_add_css('/library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.
 head_add_css('/view/theme/adminlte/css/adminlte.min.css');
 head_add_css('/view/theme/adminlte/css/overlayscrollbar.min.css');
 head_add_css('/view/theme/adminlte/css/style.css');
-
 
 head_add_js('/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js');
 head_add_js('/library/bootbox/bootbox.min.js');
