dab6eb943003c2dee4607b888a45409043ae41da
SK <sk@utsukta.org>
2025-03-13 15:20:38 +0530

    test1

--- adminlte/php/style.php
+++ adminlte/php/style.php
@@ -9,28 +9,26 @@ if(!App::$install) {
 		// Load the owners pconfig
 		load_pconfig($uid, 'adminlte');
 
-		$custom_bs = get_pconfig($uid, 'adminlte', 'bootstrap');
-		$nav_bg = get_pconfig($uid, 'adminlte', 'nav_bg');
-		$nav_bg_dark = get_pconfig($uid, 'adminlte', 'nav_bg_dark');
-		$narrow_navbar = get_pconfig($uid,'adminlte','narrow_navbar');
-		$bgcolor = get_pconfig($uid, 'adminlte', 'background_color');
-		$bgcolor_dark = get_pconfig($uid, 'adminlte', 'background_color_dark');
+		/*$custom_bs = get_pconfig($uid, 'adminlte', 'bootstrap');*/
+		/*$nav_bg = get_pconfig($uid, 'adminlte', 'nav_bg');*/
+		/*$nav_bg_dark = get_pconfig($uid, 'adminlte', 'nav_bg_dark');*/
+		/*$narrow_navbar = get_pconfig($uid,'adminlte','narrow_navbar');*/
+		/*$bgcolor = get_pconfig($uid, 'adminlte', 'background_color');*/
+		/*$bgcolor_dark = get_pconfig($uid, 'adminlte', 'background_color_dark');*/
     $schema = get_pconfig($uid,'adminlte','schema');
-    $schema = 'journal';
-		$background_image = get_pconfig($uid, 'adminlte', 'background_image');
-		$background_image_dark = get_pconfig($uid, 'adminlte', 'background_image_dark');
-		$font_size = get_pconfig($uid, 'adminlte', 'font_size');
-		$converse_width = get_pconfig($uid,'adminlte','converse_width');
-		$top_photo = get_pconfig($uid,'adminlte','top_photo');
-    $reply_photo = get_pconfig($uid,'adminlte','reply_photo');
+		/*  $schema = 'journal';*/
+		/*$background_image = get_pconfig($uid, 'adminlte', 'background_image');*/
+		/*$background_image_dark = get_pconfig($uid, 'adminlte', 'background_image_dark');*/
+		/*$font_size = get_pconfig($uid, 'adminlte', 'font_size');*/
+		/*$converse_width = get_pconfig($uid,'adminlte','converse_width');*/
+		/*$top_photo = get_pconfig($uid,'adminlte','top_photo');*/
+		/*  $reply_photo = get_pconfig($uid,'adminlte','reply_photo');*/
     echo 'uid' . $uid . 'schema:' . $schema ;
     echo 'view/theme/adminlte/schema/' . $schema . '.min.css';
 
 	}
 }
 
-$schema = 'journal';
-echo 'uid' . $uid . 'schema:' . $schema ;
 /*$left_aside_width = 21; //unit: rem*/
 /*$right_aside_width = 21; //unit: rem*/
 
@@ -39,34 +37,27 @@ echo 'uid' . $uid . 'schema:' . $schema ;
 // prevent main_width smaller than 768px
 /*$main_width = (($main_width < 30) ? 30 : $main_width);*/
 
-$options = array (
-  '$nav_bg' => $nav_bg,
-  '$nav_bg_dark' => $nav_bg_dark,
-  '$bgcolor' => $bgcolor,
-  '$bgcolor_dark' => $bgcolor_dark,
-  '$background_image' => $background_image,
-  '$background_image_dark' => $background_image_dark,
-  '$font_size' => $font_size,
-  '$converse_width' => $converse_width,
-  '$top_photo' => $top_photo,
-  '$reply_photo' => $reply_photo,
-  '$main_width' => $main_width,
-  '$left_aside_width' => $left_aside_width,
-  '$right_aside_width' => $right_aside_width
-);
-
+/*$options = array (*/
+/*  '$nav_bg' => $nav_bg,*/
+/*  '$nav_bg_dark' => $nav_bg_dark,*/
+/*  '$bgcolor' => $bgcolor,*/
+/*  '$bgcolor_dark' => $bgcolor_dark,*/
+/*  '$background_image' => $background_image,*/
+/*  '$background_image_dark' => $background_image_dark,*/
+/*  '$font_size' => $font_size,*/
+/*  '$converse_width' => $converse_width,*/
+/*  '$top_photo' => $top_photo,*/
+/*  '$reply_photo' => $reply_photo,*/
+/*  '$main_width' => $main_width,*/
+/*  '$left_aside_width' => $left_aside_width,*/
+/*  '$right_aside_width' => $right_aside_width*/
+/*);*/
+/**/
 // Apply the settings
 
 $x = file_get_contents('view/theme/adminlte/css/style.css');
 
 $schemecss = file_get_contents('view/theme/adminlte/schema/' . $schema . '.min.css');
-
-if ($schemecss === false) {
-    echo "Error: Could not load schema\n";
-
-} else {
-    echo "Loaded cosmo.min.css (" . strlen($schemecss) . " bytes)\n";
-}
 
 /*$o = strtr($x, $options);*/
 
