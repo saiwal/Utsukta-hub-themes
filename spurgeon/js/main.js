b23d5bab3849af84563d780578022a449aab9bb2
SK <sk@utsukta.org>
2025-03-30 02:53:15 +0530

    removes swiper() from main.js

--- spurgeon/js/main.js
+++ spurgeon/js/main.js
@@ -357,7 +357,7 @@
         ssSearch();
         ssMasonry();
         ssAnimateBricks();
-        ssSwiper();
+        //ssSwiper();
         ssAlertBoxes();
         ssBackToTop();
         ssMoveTo();
