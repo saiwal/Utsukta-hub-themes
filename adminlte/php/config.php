4668070564ec70580ee921ead70c933cead58a9c
SK <sk@utsukta.org>
2025-04-06 20:26:59 +0530

    test bg image

--- adminlte/php/config.php
+++ adminlte/php/config.php
@@ -61,30 +61,30 @@ class AdminlteConfig {
 
 		set_pconfig(local_channel(), 'adminlte', 'schema', $_POST['schema']);
 		set_pconfig(local_channel(), 'system', 'style_update', time());
-		/*if (isset($_POST['redbasic-settings-submit'])) {*/
-		/*	if (isset($_POST['redbasic_primary_color']) || isset($_POST['redbasic_radius'])) {*/
+		/*if (isset($_POST['adminlte-settings-submit'])) {*/
+		/*	if (isset($_POST['adminlte_primary_color']) || isset($_POST['adminlte_radius'])) {*/
 		/**/
 		/*		$primary_color = '';*/
 		/*		$success_color = '';*/
 		/*		$info_color = '';*/
 		/*		$warning_color = '';*/
 		/*		$danger_color = '';*/
-		/*		$radius = floatval($_POST['redbasic_radius']);*/
+		/*		$radius = floatval($_POST['adminlte_radius']);*/
 		/**/
-		/*		if (preg_match('/^#([A-Fa-f0-9]{3}){1,2}$/', $_POST['redbasic_primary_color'])) {*/
-		/*			$primary_color = $_POST['redbasic_primary_color'];*/
+		/*		if (preg_match('/^#([A-Fa-f0-9]{3}){1,2}$/', $_POST['adminlte_primary_color'])) {*/
+		/*			$primary_color = $_POST['adminlte_primary_color'];*/
 		/*		}*/
-		/*		if (preg_match('/^#([A-Fa-f0-9]{3}){1,2}$/', $_POST['redbasic_success_color'])) {*/
-		/*			$success_color = $_POST['redbasic_success_color'];*/
+		/*		if (preg_match('/^#([A-Fa-f0-9]{3}){1,2}$/', $_POST['adminlte_success_color'])) {*/
+		/*			$success_color = $_POST['adminlte_success_color'];*/
 		/*		}*/
-		/*		if (preg_match('/^#([A-Fa-f0-9]{3}){1,2}$/', $_POST['redbasic_info_color'])) {*/
-		/*			$info_color = $_POST['redbasic_info_color'];*/
+		/*		if (preg_match('/^#([A-Fa-f0-9]{3}){1,2}$/', $_POST['adminlte_info_color'])) {*/
+		/*			$info_color = $_POST['adminlte_info_color'];*/
 		/*		}*/
-		/*		if (preg_match('/^#([A-Fa-f0-9]{3}){1,2}$/', $_POST['redbasic_warning_color'])) {*/
-		/*			$warning_color = $_POST['redbasic_warning_color'];*/
+		/*		if (preg_match('/^#([A-Fa-f0-9]{3}){1,2}$/', $_POST['adminlte_warning_color'])) {*/
+		/*			$warning_color = $_POST['adminlte_warning_color'];*/
 		/*		}*/
-		/*		if (preg_match('/^#([A-Fa-f0-9]{3}){1,2}$/', $_POST['redbasic_danger_color'])) {*/
-		/*			$danger_color = $_POST['redbasic_danger_color'];*/
+		/*		if (preg_match('/^#([A-Fa-f0-9]{3}){1,2}$/', $_POST['adminlte_danger_color'])) {*/
+		/*			$danger_color = $_POST['adminlte_danger_color'];*/
 		/*		}*/
 		/**/
 		/*		if ($primary_color || $success_color || $info_color || $warning_color || $danger_color || $radius) {*/
@@ -97,7 +97,7 @@ class AdminlteConfig {
 		/**/
 		/*				$options = [*/
 		/*					'cacheDir' => $cache_dir,*/
-		/*					'prefix' => 'redbasic_',*/
+		/*					'prefix' => 'adminlte_',*/
 		/*					'forceRefresh' => false*/
 		/*				];*/
 		/**/
@@ -145,26 +145,26 @@ class AdminlteConfig {
 		/*	}*/
 		/**/
 		/*	set_pconfig(local_channel(), 'adminlte', 'primary_color', $_POST['adminlte_primary_color']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'success_color', $_POST['redbasic_success_color']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'info_color', $_POST['redbasic_info_color']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'warning_color', $_POST['redbasic_warning_color']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'danger_color', $_POST['redbasic_danger_color']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'success_color', $_POST['adminlte_success_color']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'info_color', $_POST['adminlte_info_color']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'warning_color', $_POST['adminlte_warning_color']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'danger_color', $_POST['adminlte_danger_color']);*/
 		/**/
-		/*	set_pconfig(local_channel(), 'adminlte', 'narrow_navbar', $_POST['redbasic_narrow_navbar']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'navbar_dark_mode', $_POST['redbasic_navbar_dark_mode']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'dark_mode', $_POST['redbasic_dark_mode']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'nav_bg', $_POST['redbasic_nav_bg']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'nav_bg_dark', $_POST['redbasic_nav_bg_dark']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'background_color', $_POST['redbasic_background_color']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'background_color_dark', $_POST['redbasic_background_color_dark']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'background_image', $_POST['redbasic_background_image']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'background_image_dark', $_POST['redbasic_background_image_dark']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'font_size', $_POST['redbasic_font_size']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'radius', $_POST['redbasic_radius']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'converse_width', $_POST['redbasic_converse_width']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'top_photo', $_POST['redbasic_top_photo']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'reply_photo', $_POST['redbasic_reply_photo']);*/
-		/*	set_pconfig(local_channel(), 'adminlte', 'advanced_theming', $_POST['redbasic_advanced_theming']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'narrow_navbar', $_POST['adminlte_narrow_navbar']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'navbar_dark_mode', $_POST['adminlte_navbar_dark_mode']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'dark_mode', $_POST['adminlte_dark_mode']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'nav_bg', $_POST['adminlte_nav_bg']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'nav_bg_dark', $_POST['adminlte_nav_bg_dark']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'background_color', $_POST['adminlte_background_color']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'background_color_dark', $_POST['adminlte_background_color_dark']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'background_image', $_POST['adminlte_background_image']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'background_image_dark', $_POST['adminlte_background_image_dark']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'font_size', $_POST['adminlte_font_size']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'radius', $_POST['adminlte_radius']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'converse_width', $_POST['adminlte_converse_width']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'top_photo', $_POST['adminlte_top_photo']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'reply_photo', $_POST['adminlte_reply_photo']);*/
+		/*	set_pconfig(local_channel(), 'adminlte', 'advanced_theming', $_POST['adminlte_advanced_theming']);*/
 		/**/
 			// This is used to refresh the cache
 			/*set_pconfig(local_channel(), 'system', 'style_update', time());*/
@@ -187,26 +187,22 @@ class AdminlteConfig {
 			'$dark' => t('Dark style'),
 			'$light' => t('Light style'),
 			'$common' => t('Common settings'),
-			/*'$primary_color' => array('redbasic_primary_color', t('Primary theme color'), $arr['primary_color'], '<i class="bi bi-circle-fill text-primary"></i> ' . t('Current color, leave empty for default')),*/
-			/*'$success_color' => array('redbasic_success_color', t('Success theme color'), $arr['success_color'], '<i class="bi bi-circle-fill text-success"></i> ' . t('Current color, leave empty for default')),*/
-			/*'$info_color' => array('redbasic_info_color', t('Info theme color'), $arr['info_color'], '<i class="bi bi-circle-fill text-info"></i> ' . t('Current color, leave empty for default')),*/
-			/*'$warning_color' => array('redbasic_warning_color', t('Warning theme color'), $arr['warning_color'], '<i class="bi bi-circle-fill text-warning"></i> ' . t('Current color, leave empty for default')),*/
-			/*'$danger_color' => array('redbasic_danger_color', t('Danger theme color'), $arr['danger_color'], '<i class="bi bi-circle-fill text-danger"></i> ' . t('Current color, leave empty for default')),*/
-			/*'$dark_mode' => array('redbasic_dark_mode',t('Default to dark mode'),$arr['dark_mode'], '', array(t('No'),t('Yes'))),*/
-			/*'$navbar_dark_mode' => array('redbasic_navbar_dark_mode',t('Always use light icons for navbar'),$arr['navbar_dark_mode'], t('Enable this option if you use a dark navbar color in light mode'), array(t('No'),t('Yes'))),*/
-			/*'$narrow_navbar' => array('redbasic_narrow_navbar',t('Narrow navbar'),$arr['narrow_navbar'], '', array(t('No'),t('Yes'))),*/
-			/*'$nav_bg' => array('redbasic_nav_bg', t('Navigation bar background color'), $arr['nav_bg']),*/
-			/*'$nav_bg_dark' => array('redbasic_nav_bg_dark', t('Dark navigation bar background color'), $arr['nav_bg_dark']),*/
-			/*'$bgcolor' => array('redbasic_background_color', t('Set the background color'), $arr['bgcolor']),*/
-			/*'$bgcolor_dark' => array('redbasic_background_color_dark', t('Set the dark background color'), $arr['bgcolor_dark']),*/
-			/*'$background_image' => array('redbasic_background_image', t('Set the background image'), $arr['background_image']),*/
-			/*'$background_image_dark' => array('redbasic_background_image_dark', t('Set the dark background image'), $arr['background_image_dark']),*/
-			/*'$font_size' => array('redbasic_font_size', t('Set font-size for the entire application'), $arr['font_size'], t('Examples: 1rem, 100%, 16px')),*/
-			/*'$radius' => array('redbasic_radius', t('Set radius of corners in rem'), $arr['radius'], t('Leave empty for default radius')),*/
-			/*'$converse_width' => array('redbasic_converse_width',t('Set maximum width of content region in rem'),$arr['converse_width'], t('Leave empty for default width')),*/
-			/*'$top_photo' => array('redbasic_top_photo', t('Set size of conversation author photo'), $arr['top_photo'], t('Leave empty for default size')),*/
-			/*'$reply_photo' => array('redbasic_reply_photo', t('Set size of followup author photos'), $arr['reply_photo'], t('Leave empty for default size')),*/
-			/*'$advanced_theming' => ['redbasic_advanced_theming', t('Show advanced settings'), $arr['advanced_theming'], '', [t('No'), t('Yes')]]*/
+			'$primary_color' => array('adminlte_primary_color', t('Primary theme color'), $arr['primary_color'], '<i class="bi bi-circle-fill text-primary"></i> ' . t('Current color, leave empty for default')),
+			'$success_color' => array('adminlte_success_color', t('Success theme color'), $arr['success_color'], '<i class="bi bi-circle-fill text-success"></i> ' . t('Current color, leave empty for default')),
+			'$info_color' => array('adminlte_info_color', t('Info theme color'), $arr['info_color'], '<i class="bi bi-circle-fill text-info"></i> ' . t('Current color, leave empty for default')),
+			'$warning_color' => array('adminlte_warning_color', t('Warning theme color'), $arr['warning_color'], '<i class="bi bi-circle-fill text-warning"></i> ' . t('Current color, leave empty for default')),
+			'$danger_color' => array('adminlte_danger_color', t('Danger theme color'), $arr['danger_color'], '<i class="bi bi-circle-fill text-danger"></i> ' . t('Current color, leave empty for default')),
+			'$dark_mode' => array('adminlte_dark_mode',t('Default to dark mode'),$arr['dark_mode'], '', array(t('No'),t('Yes'))),
+			'$bgcolor' => array('adminlte_background_color', t('Set the background color'), $arr['bgcolor']),
+			'$bgcolor_dark' => array('adminlte_background_color_dark', t('Set the dark background color'), $arr['bgcolor_dark']),
+			'$background_image' => array('adminlte_background_image', t('Set the background image'), $arr['background_image']),
+			'$background_image_dark' => array('adminlte_background_image_dark', t('Set the dark background image'), $arr['background_image_dark']),
+			'$font_size' => array('adminlte_font_size', t('Set font-size for the entire application'), $arr['font_size'], t('Examples: 1rem, 100%, 16px')),
+			'$radius' => array('adminlte_radius', t('Set radius of corners in rem'), $arr['radius'], t('Leave empty for default radius')),
+			'$converse_width' => array('adminlte_converse_width',t('Set maximum width of content region in rem'),$arr['converse_width'], t('Leave empty for default width')),
+			'$top_photo' => array('adminlte_top_photo', t('Set size of conversation author photo'), $arr['top_photo'], t('Leave empty for default size')),
+			'$reply_photo' => array('adminlte_reply_photo', t('Set size of followup author photos'), $arr['reply_photo'], t('Leave empty for default size')),
+			'$advanced_theming' => ['adminlte_advanced_theming', t('Show advanced settings'), $arr['advanced_theming'], '', [t('No'), t('Yes')]]
 			));
 
 		return $o;
@@ -214,12 +210,12 @@ class AdminlteConfig {
 
 }
 
-function redbasic_theme_admin_enable() {
+function adminlte_theme_admin_enable() {
 	// This function is called once when the theme is being enabled by the admin
 	// It can be used to register hooks etc.
 }
 
-function redbasic_theme_admin_disable() {
+function adminlte_theme_admin_disable() {
 	// This function is called once when the theme is being disabled by the admin
 	// It can be used to unregister hooks etc.
 }
