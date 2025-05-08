f488304c49b29a0888416ef43f6ca5f763f09a0f
SK <sk@utsukta.org>
2025-05-08 19:20:50 +0530

    test search

--- adminlte/php/default.php
+++ adminlte/php/default.php
@@ -64,26 +64,6 @@
     <!-- /.content-wrapper -->
 
   </div>
-<!-- Search Modal -->
-<div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="searchModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-sm">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="searchModalLabel">Search</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form class="d-flex" method="get" action="{{$nav.search.4}}" role="search">
-                    <input class="form-control form-control-sm me-2" id="nav-search-text" type="text" value="" placeholder="{{$help}}" name="search" title="{{$nav.search.3}}" />
-                    <button type="submit" class="btn btn-outline-secondary btn-sm"><i class="bi bi-search"></i></button>
-                </form>
-                <div id="nav-search-spinner" class="spinner-wrapper d-none">
-                    <div class="spinner s"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>    
   <!-- ./wrapper -->
   <script>
     // Color Mode Toggler
