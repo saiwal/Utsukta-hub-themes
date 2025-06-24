f26955b2227df063a5459babfec9e38ba82040f2
SK <sk@utsukta.org>
2025-06-24 17:52:12 +0530

    notifications fix

--- adminlte/widget/widget_mynavbar.php
+++ adminlte/widget/widget_mynavbar.php
@@ -489,163 +489,164 @@ function widget_mynavbar($args)
 
     $c = theme_include("navbar_" . purify_filename($template) . ".css");
     $tpl = get_markup_template("navbar_" . purify_filename($template) . ".tpl");
-    // notifications
 
     $channel = \App::get_channel();
-    $notifications = [];
+		$notifications = [];
 
-    if (local_channel()) {
-        $notifications[] = [
-            "type" => "network",
-            "icon" => "grid-3x3",
-            "severity" => "secondary",
-            "label" => t("Network"),
-            "title" => t("New network activity notifications"),
-            "viewall" => [
-                "url" => "network",
-                "label" => t("Network stream"),
-            ],
-            "markall" => [
-                "label" => t("Mark all notifications read"),
-            ],
-            "filter" => [
-                "posts_label" => t("Show new posts only"),
-                "name_label" => t("Filter by name or address"),
-            ],
-        ];
+		if(local_channel()) {
+			$notifications[] = [
+				'type' => 'network',
+				'icon' => 'grid-3x3',
+				'severity' => 'secondary',
+				'label' => t('Network'),
+				'title' => t('Unseen network activity'),
+				'viewall' => [
+					'url' => 'network',
+					'label' => t('Network stream')
+				],
+				'markall' => [
+					'label' => t('Mark all read')
+				],
+				'filter' => [
+					'posts_label' => t('Conversation starters'),
+					'name_label' => t('Filter by name or address')
+				]
+			];
 
-        $notifications[] = [
-            "type" => "home",
-            "icon" => "house",
-            "severity" => "danger",
-            "label" => t("Home"),
-            "title" => t("New home activity notifications"),
-            "viewall" => [
-                "url" => "channel/" . $channel["channel_address"],
-                "label" => t("Home stream"),
-            ],
-            "markall" => [
-                "label" => t("Mark all notifications seen"),
-            ],
-            "filter" => [
-                "posts_label" => t("Show new posts only"),
-                "name_label" => t("Filter by name or address"),
-            ],
-        ];
 
-        $notifications[] = [
-            "type" => "dm",
-            "icon" => "envelope",
-            "severity" => "danger",
-            "label" => t("Direct Messages"),
-            "title" => t("New direct messages notifications"),
-            "viewall" => [
-                "url" => "network/?dm=1",
-                "label" => t("Direct messages stream"),
-            ],
-            "markall" => [
-                "label" => t("Mark all notifications read"),
-            ],
-            "filter" => [
-                "posts_label" => t("Show new posts only"),
-                "name_label" => t("Filter by name or address"),
-            ],
-        ];
+			$notifications[] = [
+				'type' => 'home',
+				'icon' => 'house',
+				'severity' => 'danger',
+				'label' => t('Channel'),
+				'title' => t('Unseen channel activity'),
+				'viewall' => [
+					'url' => 'channel/' . $channel['channel_address'],
+					'label' => t('Channel stream')
+				],
+				'markall' => [
+					'label' => t('Mark all seen')
+				],
+				'filter' => [
+					'posts_label' => t('Conversation starters'),
+					'name_label' => t('Filter by name or address')
+				]
+			];
 
-        $notifications[] = [
-            "type" => "all_events",
-            "icon" => "calendar-date",
-            "severity" => "secondary",
-            "label" => t("Events"),
-            "title" => t("New events notifications"),
-            "viewall" => [
-                "url" => "cdav/calendar",
-                "label" => t("View events"),
-            ],
-            "markall" => [
-                "label" => t("Mark all events seen"),
-            ],
-        ];
+			$notifications[] = [
+				'type' => 'dm',
+				'icon' => 'envelope',
+				'severity' => 'danger',
+				'label' => t('Private'),
+				'title' => t('Unseen private activity'),
+				'viewall' => [
+					'url' => 'network/?dm=1',
+					'label' => t('Private stream')
+				],
+				'markall' => [
+					'label' => t('Mark all read')
+				],
+				'filter' => [
+					'posts_label' => t('Conversation starters'),
+					'name_label' => t('Filter by name or address')
+				]
+			];
 
-        $notifications[] = [
-            "type" => "intros",
-            "icon" => "people",
-            "severity" => "danger",
-            "label" => t("New Connections"),
-            "title" => t("New connections notifications"),
-            "viewall" => [
-                "url" => "connections",
-                "label" => t("View all connections"),
-            ],
-        ];
+			$notifications[] = [
+				'type' => 'all_events',
+				'icon' => 'calendar-date',
+				'severity' => 'secondary',
+				'label' => t('Events'),
+				'title' => t('Unseen events activity'),
+				'viewall' => [
+					'url' => 'cdav/calendar',
+					'label' => t('View events')
+				],
+				'markall' => [
+					'label' => t('Mark all seen')
+				]
+			];
 
-        $notifications[] = [
-            "type" => "files",
-            "icon" => "folder",
-            "severity" => "danger",
-            "label" => t("Files"),
-            "title" => t("New files notifications"),
-        ];
+			$notifications[] = [
+				'type' => 'intros',
+				'icon' => 'people',
+				'severity' => 'danger',
+				'label' => t('New Connections'),
+				'title' => t('New connections'),
+				'viewall' => [
+					'url' => 'connections',
+					'label' => t('View all')
+				]
+			];
 
-        $notifications[] = [
-            "type" => "notify",
-            "icon" => "exclamation-circle",
-            "severity" => "danger",
-            "label" => t("Notices"),
-            "title" => t("Notices"),
-            "viewall" => [
-                "url" => "notifications/system",
-                "label" => t("View all notices"),
-            ],
-            "markall" => [
-                "label" => t("Mark all notices seen"),
-            ],
-        ];
+			$notifications[] = [
+				'type' => 'files',
+				'icon' => 'folder',
+				'severity' => 'danger',
+				'label' => t('Files'),
+				'title' => t('Useen files activity'),
+			];
 
-        $notifications[] = [
-            "type" => "forums",
-            "icon" => "chat-quote",
-            "severity" => "secondary",
-            "label" => t("Forums"),
-            "title" => t("Forums"),
-            "filter" => [
-                "name_label" => t("Filter by name or address"),
-            ],
-        ];
-    }
+			$notifications[] = [
+				'type' => 'notify',
+				'icon' => 'exclamation-circle',
+				'severity' => 'danger',
+				'label' => t('Notifications'),
+				'title' => t('Unseen notifications'),
+				'viewall' => [
+					'url' => 'notifications/system',
+					'label' => t('View all')
+				],
+				'markall' => [
+					'label' => t('Mark all seen')
+				]
+			];
 
-    if (local_channel() && is_site_admin()) {
-        $notifications[] = [
-            "type" => "register",
-            "icon" => "person-exclamation",
-            "severity" => "danger",
-            "label" => t("Registrations"),
-            "title" => t("New registrations notifications"),
-        ];
-    }
+			$notifications[] = [
+				'type' => 'forums',
+				'icon' => 'chat-quote',
+				'severity' => 'secondary',
+				'label' => t('Forums'),
+				'title' => t('Unseen forums activity'),
+				'filter' => [
+					'name_label' => t('Filter by name or address')
+				]
+			];
+		}
 
-    if (can_view_public_stream()) {
-        $notifications[] = [
-            "type" => "pubs",
-            "icon" => "globe",
-            "severity" => "secondary",
-            "label" => t("Public Stream"),
-            "title" => t("New public stream notifications"),
-            "viewall" => [
-                "url" => "pubstream",
-                "label" => t("Public stream"),
-            ],
-            /*
+		if(local_channel() && is_site_admin()) {
+			$notifications[] = [
+				'type' => 'register',
+				'icon' => 'person-exclamation',
+				'severity' => 'danger',
+				'label' => t('Registrations'),
+				'title' => t('Unseen registration activity'),
+			];
+		}
+
+		if(can_view_public_stream()) {
+			$notifications[] = [
+				'type' => 'pubs',
+				'icon' => 'globe',
+				'severity' => 'secondary',
+				'label' => t('Public Stream'),
+				'title' => t('Unseen public stream activity'),
+				'viewall' => [
+					'url' => 'pubstream',
+					'label' => t('Public stream')
+				],
+				/*
 				'markall' => [
 					'label' => t('Mark all notifications seen')
 				],
 				*/
-            "filter" => [
-                "posts_label" => t("Show new posts only"),
-                "name_label" => t("Filter by name or address"),
-            ],
-        ];
-    }
+				'filter' => [
+					'posts_label' => t('Conversation starters'),
+					'name_label' => t('Filter by name or address')
+				]
+			];
+		}
+
 
     if ($c && $tpl) {
         head_add_css("navbar_" . $template . ".css");
