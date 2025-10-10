81b3cf3105cdcf67978adf0e12e94137276421a6
SK <sk@utsukta.org>
2025-10-10 18:59:44 +0530

    add full layout

---
+++ adminlte/php/full.php
@@ -0,0 +1,150 @@
+<?php
+
+/**
+ * * Name: full
+ *   * Description: AdminLTE fullwidth layout
+ *   * Version: 1.7
+ *   * Author: Saiwal
+ *   * Maintainer: Saiwal
+ *   * ContentRegion: content, region_2
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php if (x($page, 'title')) echo $page['title'] ?></title>
+  <script>
+    var baseurl = "<?php echo z_root() ?>";
+  </script>
+  <?php if (x($page, 'htmlhead')) echo $page['htmlhead'] ?>
+</head>
+
+  <body class="layout-fixed sidebar-expand-lg <?php echo $page['sidebar_mode'] ?> app-loaded sidebar-open">
+  <div class="app-wrapper">
+
+    <header><?php if (x($page, 'header')) echo $page['header']; ?></header>
+    
+    <?php echo x($page, 'topnav') ? $page['topnav'] : (x($page, 'nav') ? $page['nav'] : ''); ?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <main class="app-main px-1 py-3" style="min-height: calc(100vh - 56px);">
+      <div class="container-xl">
+        <div class="row">
+        <div id="region_2" class="col-12">
+          <?php if (x($page, 'content')) echo $page['content']; ?>
+        </div>
+        </div>
+      </div>
+    </main>
+    <!-- /.content-wrapper -->
+
+  </div>
+  <!-- ./wrapper -->
+  <script>
+    // Color Mode Toggler
+    (() => {
+      "use strict";
+
+      const storedTheme = localStorage.getItem("theme");
+      const defaultTheme = "<?php echo $page['color_mode'] ?? 'light'; ?>"; // Hubzilla setting
+      const getPreferredTheme = () => {
+        if (storedTheme) {
+          return storedTheme;
+        }
+        if (defaultTheme === "dark" || defaultTheme === "light" || defaultTheme === "auto") {
+          return defaultTheme;
+        }
+        return window.matchMedia("(prefers-color-scheme: dark)").matches ?
+          "dark" :
+          "light";
+      };
+
+      const setTheme = function(theme) {
+        if (theme === "auto" && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+          document.documentElement.setAttribute("data-bs-theme", "dark");
+        } else {
+          document.documentElement.setAttribute("data-bs-theme", theme);
+        }
+      };
+
+      const showActiveTheme = (theme, focus = false) => {
+        const themeSwitcher = document.querySelector("#bd-theme");
+
+        if (!themeSwitcher) {
+          return;
+        }
+
+        const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`);
+        const inputToCheck = document.querySelector(`#bd-theme input[data-bs-theme-value="${theme}"]`);
+
+        for (const element of document.querySelectorAll("[data-bs-theme-value]")) {
+          element.classList.remove("active");
+          element.setAttribute("aria-pressed", "false");
+        }
+
+        btnToActive.classList.add("active");
+        btnToActive.setAttribute("aria-pressed", "true");
+
+        // Update the checked state of the radio button
+        if (inputToCheck) {
+          inputToCheck.checked = true;
+        }
+
+        if (focus) {
+          btnToActive.focus();
+        }
+      };
+
+      setTheme(getPreferredTheme());
+
+      window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .addEventListener("change", () => {
+          if (storedTheme !== "light" || storedTheme !== "dark") {
+            setTheme(getPreferredTheme());
+          }
+        });
+
+      window.addEventListener("DOMContentLoaded", () => {
+        showActiveTheme(getPreferredTheme());
+
+        for (const toggle of document.querySelectorAll("[data-bs-theme-value]")) {
+          toggle.addEventListener("click", () => {
+            const theme = toggle.getAttribute("data-bs-theme-value");
+            localStorage.setItem("theme", theme);
+            setTheme(theme);
+            showActiveTheme(theme, true);
+          });
+        }
+      });
+    })();
+  </script>
+  <script>
+    const SELECTOR_SIDEBAR_WRAPPER = ".sidebar-wrapper";
+    const Default = {
+      scrollbarTheme: "os-theme-light",
+      scrollbarAutoHide: "leave",
+      scrollbarClickScroll: true,
+    };
+    document.addEventListener("DOMContentLoaded", function() {
+      const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
+      if (
+        sidebarWrapper &&
+        typeof OverlayScrollbarsGlobal?.OverlayScrollbars !== "undefined"
+      ) {
+        OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
+          scrollbars: {
+            theme: Default.scrollbarTheme,
+            autoHide: Default.scrollbarAutoHide,
+            clickScroll: Default.scrollbarClickScroll,
+          },
+        });
+      }
+    });
+  </script> <!--end::OverlayScrollbars Configure-->
+</body>
+
+</html>
