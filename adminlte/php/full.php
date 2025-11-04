35dda5c2bfa8099cb268a2315b2c671202ac90ec
SK <sk@utsukta.org>
2025-11-04 15:59:42 +0530

    version update

--- adminlte/php/full.php
+++ adminlte/php/full.php
@@ -3,7 +3,7 @@
 /**
  * * Name: full
  *   * Description: AdminLTE fullwidth layout
- *   * Version: 1.7
+ *   * Version: 1.8
  *   * Author: Saiwal
  *   * Maintainer: Saiwal
  *   * ContentRegion: content, region_2
