77a30a100c2518ebd373d7137675f9b342a3da36
SK <sk@utsukta.org>
2026-01-22 14:31:25 +0530

    implement banner image

--- keepitsimple/php/style.php
+++ keepitsimple/php/style.php
@@ -1,5 +1,6 @@
 <?php
 
+use Zotlabs\Lib\Config;
 if(!App::$install) {
 
 	// Get the UID of the channel owner
@@ -8,49 +9,17 @@ if(!App::$install) {
 	if($uid) {
 		// Load the owners pconfig
 		load_pconfig($uid, 'keepitsimple');
-
-		/*$custom_bs = get_pconfig($uid, 'adminlte', 'bootstrap');*/
-		/*$nav_bg = get_pconfig($uid, 'adminlte', 'nav_bg');*/
-		/*$nav_bg_dark = get_pconfig($uid, 'adminlte', 'nav_bg_dark');*/
-		/*$narrow_navbar = get_pconfig($uid,'adminlte','narrow_navbar');*/
-		/*$bgcolor = get_pconfig($uid, 'adminlte', 'background_color');*/
-		/*$bgcolor_dark = get_pconfig($uid, 'adminlte', 'background_color_dark');*/
-    /*$schema = get_pconfig($uid, 'adminlte', 'schema');*/
-    /*var_dump(App::$config[$uid]['adminlte']); // Check the 'adminlte' family*/
-    /*var_dump(App::$config[$uid]['adminlte']['schema']); // Check the 'schema' key    */
-		/*  $schema = 'journal';*/
-		/*$background_image = get_pconfig($uid, 'adminlte', 'background_image');*/
-		/*$background_image_dark = get_pconfig($uid, 'adminlte', 'background_image_dark');*/
-		/*$font_size = get_pconfig($uid, 'adminlte', 'font_size');*/
-		/*$converse_width = get_pconfig($uid,'adminlte','converse_width');*/
-		/*$top_photo = get_pconfig($uid,'adminlte','top_photo');*/
-		/*  $reply_photo = get_pconfig($uid,'adminlte','reply_photo');*/
+		$banner_image = get_pconfig($uid, 'keepitsimple', 'banner_image');
 	}
 }
 
-/*$left_aside_width = 21; //unit: rem*/
-/*$right_aside_width = 21; //unit: rem*/
-
-/*$main_width = $left_aside_width + $right_aside_width + intval($converse_width);*/
-
-// prevent main_width smaller than 768px
-/*$main_width = (($main_width < 30) ? 30 : $main_width);*/
-
-/*$options = array (*/
-/*  '$nav_bg' => $nav_bg,*/
-/*  '$nav_bg_dark' => $nav_bg_dark,*/
-/*  '$bgcolor' => $bgcolor,*/
-/*  '$bgcolor_dark' => $bgcolor_dark,*/
-/*  '$background_image' => $background_image,*/
-/*  '$background_image_dark' => $background_image_dark,*/
-/*  '$font_size' => $font_size,*/
-/*  '$converse_width' => $converse_width,*/
-/*  '$top_photo' => $top_photo,*/
-/*  '$reply_photo' => $reply_photo,*/
-/*  '$main_width' => $main_width,*/
-/*  '$left_aside_width' => $left_aside_width,*/
-/*  '$right_aside_width' => $right_aside_width*/
-/*);*/
+$sys = [
+	'banner_image'		=> Config::Get('theme_keepitsimple','banner_image','/view/theme/keepitsimple/img/header-content-bg.png'),
+];
+$banner_image = $banner_image ?: $sys['banner_image'] ?: '';
+$options = array (
+  '$banner_image' => $banner_image,
+);
 /**/
 // Allow layouts to over-ride the schema
 if (isset($_REQUEST['schema']) && preg_match('/^[\w_-]+$/i', $_REQUEST['schema'])) {
@@ -58,15 +27,15 @@ if (isset($_REQUEST['schema']) && preg_match('/^[\w_-]+$/i', $_REQUEST['schema']
 }
 // Apply the settings
 
-/*$x = file_get_contents('view/theme/adminlte/css/style.css');*/
+$x = file_get_contents('view/theme/keepitsimple/css/style.css');
 
-/*$schemecss = file_get_contents('view/theme/adminlte/schema/' . $schema . '.css');*/
+$schemecss = file_get_contents('view/theme/keepitsimple/schema/' . $schema . '.css');
 
-/*$o = strtr($x, $options);*/
+$x = strtr($x, $options);
 
 header('Cache-Control: max-age=2592000');
 
-/*echo $x . $schemecss;*/
+echo $schemecss . $x;
 
 // ! If you change the name of the directory containing the theme, be sure to change this line to match.
 /*echo @file_get_contents('/view/theme/adminlte/css/style.css');*/
--- keepitsimple/tpl/theme_settings.tpl
+++ keepitsimple/tpl/theme_settings.tpl
@@ -1,24 +1,4 @@
-	<h3>Coming soon...</h3>
-<!--{{if $expert}}-->
-<!--	<hr>-->
-<!--	<h3>{{$common}}</h3>-->
-<!--	{{include file="field_colorinput.tpl" field=$primary_color}}-->
-<!--	{{include file="field_colorinput.tpl" field=$success_color}}-->
-<!--	{{include file="field_colorinput.tpl" field=$info_color}}-->
-<!--	{{include file="field_colorinput.tpl" field=$warning_color}}-->
-<!--	{{include file="field_colorinput.tpl" field=$danger_color}}-->
-<!---->
-<!--<script>-->
-<!--	$(function(){-->
-<!--		$('#id_redbasic_link_color, #id_redbasic_link_color_dark, #id_redbasic_link_hover_color, #id_redbasic_link_hover_color_dark, #id_redbasic_background_color, #id_redbasic_background_color_dark, #id_redbasic_nav_bg, #id_redbasic_nav_bg_dark').colorpicker({format: 'rgba'});-->
-<!--	});-->
-<!--	$(function(){-->
-<!--		$('#id_redbasic_primary_color, #id_redbasic_success_color, #id_redbasic_info_color, #id_redbasic_warning_color, #id_redbasic_danger_color').colorpicker({format: 'hex'});-->
-<!--	});-->
-<!--</script>-->
-<!--{{/if}}-->
-<!---->
-
-<div class="settings-submit-wrapper" >
-	<button type="submit" name="adminlte-settings-submit" class="btn btn-primary">{{$submit}}</button>
+{{include file="field_input.tpl" field=$banner_image}}
+<div class="settings-submit-wrapper">
+	<button type="submit" name="keepitsimple-settings-submit" class="btn btn-primary">{{$submit}}</button>
 </div>
