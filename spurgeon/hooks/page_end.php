d113f3b55de7c139f06705c7540a519bbf7eabdf
SK <sk@utsukta.org>
2026-02-05 17:56:36 +0530

    cleanup

--- spurgeon/hooks/page_end.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-
-function spurgeon_test_page_end(&$o) {
-    logger('spurgeon_test_page_end fired', LOGGER_DEBUG);
-    $o .= "<div style='padding:10px;background:#eee;border:1px solid #ccc;'>Test hook output from Spurgeon theme</div>";
-}
