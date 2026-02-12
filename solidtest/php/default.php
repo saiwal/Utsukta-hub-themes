a8aeadca7b951ae57f3a30aeb9edc5da067e75fb
SK <sk@utsukta.org>
2026-02-12 20:39:44 +0530

    unocss

---
+++ solidtest/php/default.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ *   * Name: default
+ *   * Description: LCARS classic layout
+ *   * Version: 0.0
+ *   * Author: Saiwal
+ *   * Maintainer: Saiwal
+ *   * ContentRegion: aside, right_aside_wrapper
+ *   * ContentRegion: right_aside, left_aside_wrapper
+ *   * ContentRegion: content, region_2
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php if (x($page, 'title')) echo $page['title'] ?></title>
+	<script>
+    var baseurl = "<?php echo z_root() ?>";
+  </script>
+  <link rel="stylesheet" href="/view/theme/solidtest/assets/index.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="format-detection" content="date=no">
+</head>
+
+<body>
+
+<div id="root"></div>
+
+<script type="module" src="/view/theme/solidtest/assets/index.js"></script>
+
+</body>
+</html>
---
+++ solidtest/php/theme.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ *   * Name: SolidTest
+ *   * Description: Solidtest theme
+ *   * Version: 1.0-beta
+ *   * MinVersion: 11.0
+ *   * MaxVersion: 12.0
+ *   * Author: Saiwal sk@utsukta.org
+ *   * Maintainer: Saiwal sk@utsukta.org
+ *   * Compat: Hubzilla [*]
+ *
+ */
+
+function solidtest_init() {
+}
