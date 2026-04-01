bd45e593b13fe2b996aa9ba6aa77b13cf5f4d9c5
SK <sk@utsukta.org>
2026-04-01 16:13:20 +0530

    build time

--- solidified/php/default.php
+++ solidified/php/default.php
@@ -11,7 +11,13 @@
  *   * ContentRegion: content, region_2
  */
 ?>
+<?php 
+$theme_path = dirname(__FILE__);
+$assets_path = $theme_path . '/assets';
 
+$js_mtime  = @filemtime($assets_path . '/app.js')  ?: time();
+$css_mtime = @filemtime($assets_path . '/app.css') ?: time();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,7 +25,7 @@
 	<script>
     var baseurl = "<?php echo z_root() ?>";
   </script>
-  <link rel="stylesheet" href="/view/theme/solidified/assets/app.css">
+  <link rel="stylesheet" href="/view/theme/solidified/assets/app.css?v=<?php echo $css_mtime ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 	<meta name="format-detection" content="telephone=no">
 	<meta name="format-detection" content="date=no">
@@ -29,7 +35,7 @@
 
 <div id="root"></div>
 
-<script type="module" src="/view/theme/solidified/assets/app.js"></script>
+<script type="module" src="/view/theme/solidified/assets/app.js?v=<?php echo $js_mtime ?>"></script>
 
 </body>
 </html>
