2b250fd15e3d45e77fc85fb94b5e1ccb7fc9aeba
SK <sk@utsukta.org>
2026-05-09 21:49:33 +0530

    help

--- solidified/Api/Router.php
+++ solidified/Api/Router.php
@@ -11,6 +11,7 @@ class Router
         'csrf' => Handlers\Csrf::class,
         'siteinfo' => Handlers\Siteinfo::class,
         'settings' => Handlers\Settings::class,
+        'help' => Handlers\Help::class,
         'display' => Handlers\Display::class,
         'pubsites' => Handlers\Pubsites::class,
         'manage' => Handlers\Manage::class,
