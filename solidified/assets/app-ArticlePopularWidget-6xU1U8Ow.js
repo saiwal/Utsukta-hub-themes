fbd6cd5c550bdc3d20d8d9885f2fc3c51fec08eb
SK <sk@utsukta.org>
2026-07-06 15:29:19 +0530

    layout

--- solidified/assets/app-ArticlePopularWidget-6xU1U8Ow.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{Vn as t}from"./app-DdJvGkWv.js";import{t as n}from"./app-PopularPostsWidget-BYh5JXPM.js";function r(){return e(n,{get channelNick(){return t()()},type:`articles`})}export{r as default};
---
+++ solidified/assets/app-ArticlePopularWidget-DbdZ_CRU.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{Vn as t}from"./app-CTylyxh_.js";import{t as n}from"./app-PopularPostsWidget-1jysCOpX.js";function r(){return e(n,{get channelNick(){return t()()},type:`articles`})}export{r as default};
