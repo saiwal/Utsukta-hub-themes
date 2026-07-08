e071479c75d8f0900995fc0a8964aca83f9328cb
SK <sk@utsukta.org>
2026-07-08 08:16:16 +0530

    show edited

---
+++ solidified/assets/app-ArticleTagWidget-BeAdik5r.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{zn as t}from"./app-n-ohgbiO.js";import{n,u as r}from"./app-store-vg3Ja5Ka.js";import{t as i}from"./app-TagWidget-ufL8h-VB.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeTag(){return n()},onTagClick:e=>r(`tag`,e)})}export{a as default};
