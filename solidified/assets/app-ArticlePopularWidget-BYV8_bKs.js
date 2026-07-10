b28e14aea0e389ca9d9dccab398e33ac16445eec
SK <sk@utsukta.org>
2026-07-10 13:57:37 +0530

    toast fix reload

--- solidified/assets/app-ArticlePopularWidget-BYV8_bKs.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{zn as t}from"./app-DJF5bWjv.js";import{t as n}from"./app-PopularPostsWidget-D_RAuOYY.js";function r(){return e(n,{get channelNick(){return t()()},type:`articles`})}export{r as default};
