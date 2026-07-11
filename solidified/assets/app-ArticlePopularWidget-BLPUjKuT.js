120b8bc37048c1cd64cc434109f2224d56f86620
SK <sk@utsukta.org>
2026-07-11 17:47:22 +0530

    post notify

---
+++ solidified/assets/app-ArticlePopularWidget-BLPUjKuT.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{Gn as t}from"./app-BqvN40Tz.js";import{t as n}from"./app-PopularPostsWidget-BTUZgFlu.js";function r(){return e(n,{get channelNick(){return t()()},type:`articles`})}export{r as default};
