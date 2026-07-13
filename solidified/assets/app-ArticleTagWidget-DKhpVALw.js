612eea401e501c9f0f6df2c94320b1f69cf6cd43
SK <sk@utsukta.org>
2026-07-13 05:48:05 +0530

    dm composer, revision

---
+++ solidified/assets/app-ArticleTagWidget-DKhpVALw.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{Zn as t}from"./app-DLAOPL5V.js";import{n,u as r}from"./app-store-BX8bW7-f.js";import{t as i}from"./app-TagWidget-DiIAm1ql.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeTag(){return n()},onTagClick:e=>r(`tag`,e)})}export{a as default};
