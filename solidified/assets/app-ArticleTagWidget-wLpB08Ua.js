4b3a52274dad93a42b2500f9c415ad06a19aa5d2
SK <sk@utsukta.org>
2026-07-14 23:25:54 +0530

    admin logo

---
+++ solidified/assets/app-ArticleTagWidget-wLpB08Ua.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{nr as t}from"./app-BfK2L0xQ.js";import{n,u as r}from"./app-store-IAZdNik-.js";import{t as i}from"./app-TagWidget-zdv2DQ6X.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeTag(){return n()},onTagClick:e=>r(`tag`,e)})}export{a as default};
