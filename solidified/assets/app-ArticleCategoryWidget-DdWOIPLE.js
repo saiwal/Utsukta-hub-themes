5ebdba579f6466b796755d15da95f2f203e6877b
SK <sk@utsukta.org>
2026-07-14 11:09:57 +0530

    dm fix

--- solidified/assets/app-ArticleCategoryWidget-DdWOIPLE.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{er as t}from"./app-DX_PfZsK.js";import{t as n,u as r}from"./app-store-xAetoKMb.js";import{t as i}from"./app-CategoryWidget-Bng4Tlu5.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeSlug(){return n()},onCategoryClick:e=>r(`cat`,e)})}export{a as default};
