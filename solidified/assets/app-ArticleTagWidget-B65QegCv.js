22039b653f6c4d13ad32e2e8d89c83d0c5ece74e
SK <sk@utsukta.org>
2026-07-16 20:32:16 +0530

    local delete

--- solidified/assets/app-ArticleTagWidget-B65QegCv.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{nr as t}from"./app-CoQN9EkZ.js";import{n,u as r}from"./app-store-BVdejtt0.js";import{t as i}from"./app-TagWidget-CCx3ildP.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeTag(){return n()},onTagClick:e=>r(`tag`,e)})}export{a as default};
