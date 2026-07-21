a01bc1e4bb0f0e89c0faddc1c5a23ce45aca8a5b
SK <sk@utsukta.org>
2026-07-21 17:14:27 +0530

    menu overflow fix

---
+++ solidified/assets/app-ArticleCategoryWidget-XvsHKLNU.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{yr as t}from"./app-Dzacn0Nu.js";import{t as n,u as r}from"./app-store-BWrS3tvN.js";import{t as i}from"./app-CategoryWidget-C871pvO4.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeSlug(){return n()},onCategoryClick:e=>r(`cat`,e)})}export{a as default};
