20b01ec45f85ad712583c8c90b96e85cb946330c
SK <sk@utsukta.org>
2026-07-23 18:29:27 +0530

    misc bugs

--- solidified/assets/app-ArticleTagWidget-Ba-lRK0-.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{Tr as t}from"./app-C22myMjt.js";import{n,u as r}from"./app-store-sa7QDULy.js";import{t as i}from"./app-TagWidget-DiLn7NGZ.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeTag(){return n()},onTagClick:e=>r(`tag`,e)})}export{a as default};
--- solidified/assets/app-ChannelPopularWidget-BdXDvWEe.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{Tr as t}from"./app-C22myMjt.js";import{t as n}from"./app-PopularPostsWidget-SmqLfa94.js";function r(){return e(n,{get channelNick(){return t()()},type:`posts`})}export{r as default};
