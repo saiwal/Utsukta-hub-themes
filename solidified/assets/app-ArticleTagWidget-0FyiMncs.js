3a33aec82534542a36ca049a6172410be7388a1d
SK <sk@utsukta.org>
2026-07-25 14:48:28 +0530

    reactive features

--- solidified/assets/app-ArticleTagWidget-0FyiMncs.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{Tr as t}from"./app-CmpD-S9c.js";import{n,u as r}from"./app-store-f0SMwdA2.js";import{t as i}from"./app-TagWidget-Clp2jRuB.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeTag(){return n()},onTagClick:e=>r(`tag`,e)})}export{a as default};
---
+++ solidified/assets/app-ChannelPopularWidget-Dv5TK7b8.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{Tr as t}from"./app-Djp2kam8.js";import{t as n}from"./app-PopularPostsWidget-CBdGTcJf.js";function r(){return e(n,{get channelNick(){return t()()},type:`posts`})}export{r as default};
