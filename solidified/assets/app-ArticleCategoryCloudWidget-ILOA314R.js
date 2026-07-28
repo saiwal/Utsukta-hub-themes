d69540b2fcc908aff351e70920c74c54c7026b5f
SK <sk@utsukta.org>
2026-07-28 20:20:23 +0530

    editor cleanup

--- solidified/assets/app-ArticleCategoryCloudWidget-ILOA314R.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{Tr as t}from"./app-C5mqc_pA.js";import{p as n,t as r}from"./app-store-DxPJ6DWd.js";import{t as i}from"./app-CategoryCloudWidget-DuBRzBds.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeSlug(){return r()},onCategoryClick:e=>n(`cat`,e)})}export{a as default};
