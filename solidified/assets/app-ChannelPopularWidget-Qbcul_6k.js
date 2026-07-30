b537b5c4776c2c70e640a01f0b1c49c2aebb0197
SK <sk@utsukta.org>
2026-07-30 12:05:57 +0530

    draft widgets

--- solidified/assets/app-ChannelPopularWidget-Qbcul_6k.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{Gr as t}from"./app-D0kEYI9o.js";import{t as n}from"./app-PopularPostsWidget-DLALipYs.js";function r(){return e(n,{get channelNick(){return t()()},type:`posts`})}export{r as default};
