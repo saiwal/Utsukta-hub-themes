935d84a977cbf65e6455ec28c3b3ee6948cd0b18
SK <sk@utsukta.org>
2026-07-30 15:16:09 +0530

    siteinfo logo

---
+++ solidified/assets/app-ArticleCategoryCloudWidget-5QcvpqKY.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{qr as t}from"./app-CQB5tiF6.js";import{p as n,t as r}from"./app-store-DYLFIoIK.js";import{t as i}from"./app-CategoryCloudWidget-CfBMWs9i.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeSlug(){return r()},onCategoryClick:e=>n(`cat`,e)})}export{a as default};
--- solidified/assets/app-RmagicView-CSh0DH01.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{mi as t}from"./app-r9yZ2zHF.js";function n(){return e(t,{href:`/login`})}export{n as default};
