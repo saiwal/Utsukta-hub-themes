6e62c8404d082218f0e2dab47452eaa103ce9d0d
SK <sk@utsukta.org>
2026-08-02 17:42:23 +0530

    calendar event list

--- solidified/assets/app-ArticleTagWidget-BFGIOEQy.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{Xr as t}from"./app-DOIbvF1w.js";import{a as n,m as r}from"./app-store-BIlSaS55.js";import{t as i}from"./app-TagWidget-D5BHcdiY.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeTag(){return n()},onTagClick:e=>r(`tag`,e)})}export{a as default};
---
+++ solidified/assets/app-ChannelPopularWidget-rIsiNQoJ.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{Xr as t}from"./app-D8lh-o2q.js";import{t as n}from"./app-PopularPostsWidget-DbIe8Toy.js";function r(){return e(n,{get channelNick(){return t()()},type:`posts`})}export{r as default};
