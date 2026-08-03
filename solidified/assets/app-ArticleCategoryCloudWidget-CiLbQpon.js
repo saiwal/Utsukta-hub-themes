c5fbc42f171d57df940bfee26ce065c461099ad6
SK <sk@utsukta.org>
2026-08-03 18:23:02 +0530

    event render/edit fix

---
+++ solidified/assets/app-ArticleCategoryCloudWidget-CiLbQpon.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{Zr as t}from"./app-DFMMpQzj.js";import{m as n,t as r}from"./app-store-fZ0TzSU0.js";import{t as i}from"./app-CategoryCloudWidget-D0ea-jKv.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeSlug(){return r()},onCategoryClick:e=>n(`cat`,e)})}export{a as default};
