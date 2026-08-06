62c1cda6b5b9b26186f5359ddfd026cfc87d8bd3
SK <sk@utsukta.org>
2026-08-06 12:50:34 +0530

    fix chat emoji rendering

--- solidified/assets/app-ArticleCategoryWidget-WVDXJ3uU.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{li as t}from"./app-CwJMSQS-.js";import{m as n,t as r}from"./app-store-CnexpvhF.js";import{t as i}from"./app-CategoryWidget-YpgMtpNb.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeSlug(){return r()},onCategoryClick:e=>n(`cat`,e)})}export{a as default};
