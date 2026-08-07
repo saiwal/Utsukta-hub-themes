2e05039c13a5bbae5aa500217f16dcec5fcd04ed
SK <sk@utsukta.org>
2026-08-08 04:56:46 +0530

    fix event notification link

---
+++ solidified/assets/app-ArticleTagWidget-D_aOAbX_.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{ui as t}from"./app-BJFj5hDD.js";import{a as n,m as r}from"./app-store-CunZ6e0c.js";import{t as i}from"./app-TagWidget-BAd0UNDv.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeTag(){return n()},onTagClick:e=>r(`tag`,e)})}export{a as default};
--- solidified/assets/app-HqNoticesWidget-DJKA49FZ.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{t}from"./app-MessageFeed-B6aI-0c_.js";function n(){return e(t,{type:`notification`})}export{n as default};
