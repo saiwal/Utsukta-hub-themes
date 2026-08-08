3da4101981f9c25cbd420f97885b40387bc0dac8
SK <sk@utsukta.org>
2026-08-08 19:57:39 +0530

    site upgrade info

---
+++ solidified/assets/app-ArticleCategoryWidget-D_cXCLCd.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{li as t}from"./app-BQYWxE4q.js";import{m as n,t as r}from"./app-store-Cn6qF6IW.js";import{t as i}from"./app-CategoryWidget-CjefYsq5.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeSlug(){return r()},onCategoryClick:e=>n(`cat`,e)})}export{a as default};
---
+++ solidified/assets/app-ArticlePopularWidget-C8H0ISGT.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{li as t}from"./app-BQYWxE4q.js";import{t as n}from"./app-PopularPostsWidget-BAHzYIOg.js";function r(){return e(n,{get channelNick(){return t()()},type:`articles`})}export{r as default};
