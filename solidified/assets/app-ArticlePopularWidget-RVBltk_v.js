b3942282a1a63bf53c22a724e07558110e1c4df3
SK <sk@utsukta.org>
2026-08-10 03:58:44 +0530

    connection request modal link

--- solidified/assets/app-ArticlePopularWidget-RVBltk_v.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{Di as t}from"./app-BgdqzCe5.js";import{t as n}from"./app-PopularPostsWidget-DZG_ROj2.js";function r(){return e(n,{get channelNick(){return t()()},type:`articles`})}export{r as default};
---
+++ solidified/assets/app-ArticleTagWidget-CdIXyd-Y.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{Di as t}from"./app-guTfkjZp.js";import{a as n,m as r}from"./app-store-5v1ulZv3.js";import{t as i}from"./app-TagWidget-BAAhZxIb.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeTag(){return n()},onTagClick:e=>r(`tag`,e)})}export{a as default};
