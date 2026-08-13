3c50c780b5b9a06bdd962206952751ba93bf46b2
SK <sk@utsukta.org>
2026-08-13 14:39:20 +0530

    fix toc layout

--- solidified/assets/app-ArticleCategoryWidget-BwYn2f2y.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{Mi as t}from"./app-CcsmS7H4.js";import{m as n,t as r}from"./app-store-4JpHT5TW.js";import{t as i}from"./app-CategoryWidget-L90677PE.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeSlug(){return r()},onCategoryClick:e=>n(`cat`,e)})}export{a as default};
--- solidified/assets/app-HqNoticesWidget-BeRHCU3Q.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-9kNHfgxH.js";import{t}from"./app-MessageFeed-Bh8t3cWj.js";function n(){return e(t,{type:`notification`})}export{n as default};
