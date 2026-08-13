b98204498d548e1e9b48797555b3dab1f19b2d8c
SK <sk@utsukta.org>
2026-08-13 15:01:43 +0530

    slot notice

---
+++ solidified/assets/app-ArticleCategoryCloudWidget-BNrrPJNS.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{Mi as t}from"./app-CWF2CHEg.js";import{m as n,t as r}from"./app-store-Cyh0mkvj.js";import{t as i}from"./app-CategoryCloudWidget--H457T3g.js";function a(){return e(i,{get channelNick(){return t()()},type:`articles`,get activeSlug(){return r()},onCategoryClick:e=>n(`cat`,e)})}export{a as default};
---
+++ solidified/assets/app-ArticlePopularWidget-DJHwMvrS.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-9kNHfgxH.js";import{Mi as t}from"./app-CWF2CHEg.js";import{t as n}from"./app-PopularPostsWidget--AEWMo05.js";function r(){return e(n,{get channelNick(){return t()()},type:`articles`})}export{r as default};
