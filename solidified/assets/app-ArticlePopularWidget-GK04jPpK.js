30a5967522e8a5b00cf1d49caa9cfc0ada66d52e
SK <sk@utsukta.org>
2026-09-22 01:03:07 +0530

    0.5.2b

--- solidified/assets/app-ArticlePopularWidget-GK04jPpK.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-BeXRvOD7.js";import{Xt as t}from"./app-B7aeYhbv.js";import{t as n}from"./app-PopularPostsWidget-C9CpLEWg.js";function r(){return e(n,{get channelNick(){return t()()},type:`articles`})}export{r as default};
---
+++ solidified/assets/app-ChannelDetailsWidget-Bf6rbzIY.js
@@ -0,0 +1,1 @@
+import{O as e}from"./app-web-BeXRvOD7.js";import{t}from"./app-ProfileView-o6hOy46E.js";function n(){return e(t,{})}export{n as default};
