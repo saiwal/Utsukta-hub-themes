68006986b879544f200b1558534fa98633c88c30
SK <sk@utsukta.org>
2026-09-21 10:15:37 +0530

    fix avatar transparency in inbox list

--- solidified/assets/app-RmagicView-CqPSlpnM.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./app-web-BeXRvOD7.js";import{$t as t}from"./app-H1cD-z0I.js";function n(){return e(t,{href:`/login`})}export{n as default};
--- solidified/assets/app-image-GAAHSSAO-DVC1fRaF.js
+++
@@ -1,1 +0,0 @@
-import{H as e,V as t}from"./app-ExcalidrawCanvas-D0YmyJXL.js";export{e as decodePngMetadata,t as encodePngMetadata};
